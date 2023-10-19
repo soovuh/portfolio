cc6dc13304eb13ef15fc7d7b00c9fda448203666
Klymenko Illia <sovuh2703@gmail.com>
2023-10-19 17:13:59 +0300

    created basic styles for text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import base_styles from "../styles/base.module.css";
 import styles from "../styles/home.module.css";
 import photo from "../official-crop.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -10,13 +11,18 @@ const Home = () => {
           <img className={styles.image} src={photo} />
         </div>
         <div className={styles.text}>
-          <p>
-            Hi, I'm Illia Klymenko Backend Web Developer I have been actively
-            engaged in programming for 1 year. During this time, I have learned
-            and applied several technologies and approaches to creating web
-            applications. I continuously enhance my knowledge every day and am
-            excited to work on even more interesting projects.
-          </p>
+          <h1>Hi, I'm Illia Klymenko</h1>
+          <h2>Backend Web Developer</h2>
+          <div className={styles.about}>
+            I have been actively engaged in programming for 1 year. During this
+            time, I have learned and applied several technologies and approaches
+            to creating web applications. I continuously enhance my knowledge
+            every day and am excited to work on even more interesting projects.
+          </div>
+          <div className={styles.buttons}>
+            <Link to="/contacts">Contact me</Link>
+            <Link to="/portfolio">My projects</Link>
+          </div>
         </div>
       </div>
     </div>
--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -5,7 +5,6 @@ import styles from "../styles/base.module.css";
 
 const Layout = (props) => {
   const location = useLocation();
-  console.log(`Current loccation is ${location.pathname}`);
   return (
     <div className={styles.app}>
       <Navbar path={location.pathname} />
